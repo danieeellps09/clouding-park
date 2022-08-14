23e34c628fca3d0066d0421c54f8c2fc44399973
daniel lopes <danieltricolorlps@gmail.com>
2022-08-14 00:32:25 -0300

    deploy inicial

---
+++ src/main/java/com/examplecloud/desafio/cloudingparking/CloudingParkingApplication.java
@@ -0,0 +1,13 @@
+package com.examplecloud.desafio.cloudingparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudingParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudingParkingApplication.class, args);
+	}
+
+}
